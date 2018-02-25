130a0b3d30a7b2134fa25d5ef50fed04cf86e75e
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2018-02-25 18:57:44 -0500

    add title
    would something like this be fine for adding title

--- input-method.php
+++ input-method.php
@@ -1,19 +1,21 @@
 <?php
+    include 'config.php';
     include 'functions.php';
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
     
     $searchType = $_REQUEST['Digits'];
-    
+    $introTitle = $_REQUEST['title'];
     if ($searchType == "1") {
-        $action = "someone to talk to";
+        $searchDescription = "someone to talk to";
     } else {
-        $action = "meetings";
+        $searchDescription = "meetings";
     }
 ?>
 <Response>
-    <Gather numDigits="1" timeout="10000" action="input-method-result.php?SearchType=<?php echo $searchType ?>" method="GET">
-        <Say>Press 1 to search for <?php echo $action ?> by city or county name.</Say>
-        <Say>Press 2 to search for <?php echo $action ?> by zip code.</Say>
+    <Gather numDigits="1" timeout="10" action="input-method-result.php?SearchType=<?php echo $searchType ?>" method="GET">
+       <Say voice="<?php echo $voice; ?>" language="<?php echo $language; ?>"><?php echo $introTitle ?></Say>
+        <Say voice="<?php echo $voice; ?>" language="<?php echo $language; ?>">Press 1 to search for <?php echo $searchDescription ?> by city or county name.</Say>
+        <Say voice="<?php echo $voice; ?>" language="<?php echo $language; ?>">Press 2 to search for <?php echo $searchDescription ?> by zip code.</Say>
     </Gather>
 </Response>
