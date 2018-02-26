ec9ca72437c0e88514781a582cdab2f6bef42d4d
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2018-02-25 20:41:25 -0500

    Update input-method.php

--- input-method.php
+++ input-method.php
@@ -5,7 +5,8 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
     
     $searchType = $_REQUEST['Digits'];
-    
+    $inputTitle = $_REQUEST['Title'];
+
     if ($searchType == "1") {
         $searchDescription = "someone to talk to";
     } else {
@@ -14,6 +15,7 @@
 ?>
 <Response>
     <Gather numDigits="1" timeout="10" action="input-method-result.php?SearchType=<?php echo $searchType ?>" method="GET">
+        <Say voice="<?php echo $voice; ?>" language="<?php echo $language; ?>"><?php echo $inputTitle ?></Say>
         <Say voice="<?php echo $voice; ?>" language="<?php echo $language; ?>">Press 1 to search for <?php echo $searchDescription ?> by city or county name.</Say>
         <Say voice="<?php echo $voice; ?>" language="<?php echo $language; ?>">Press 2 to search for <?php echo $searchDescription ?> by zip code.</Say>
     </Gather>
